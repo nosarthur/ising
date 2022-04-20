9286cb3e00577aee06bdd043b0dc91a102deb5b1
Dong Zhou <zhou.dong@gmail.com>
2022-04-20 02:20:16 -0400

    use index 1..N for the spin config

--- magnets/magnets.go
+++ magnets/magnets.go
@@ -7,26 +7,30 @@ import (
 
 type Magnet interface {
 	// Step()
+	DeltaE(i uint) float64
 	Energy() float64
 	Magnetization() float64
 	Set(spint)
 	Show()
 }
 
+// 1D Ising magnet with PBC
 type magnet struct {
 	// the size of Spins needs to be longer than N + 2 to deal with PBC
-	spins  spint
-	N      uint // number of spins
-	J, H   float64
-	masks1 []spint // select single spin, i in 0..N-1
-	masks2 []spint // select 2 neighboring spins (i,i+1)
-	masks3 []spint // select 3 neighboring spins (i-1, i, i+1), i in 1..N
-	mask   spint   // mask all spins
+	spins    spint // spin configuration
+	N        uint  // number of spins
+	J, H     float64
+	masks1   []spint // select single spin, i in 0..N-1
+	masks2   []spint // select 2 neighboring spins (i,i+1)
+	masks3   []spint // select 3 neighboring spins (i-1, i, i+1), i in 1..N
+	mask     spint   // mask all spins
+	dE_table [8]float64
 }
 
 // Create new system (spin state not initialized)
 func New1DIsing(spins spint, N uint, J, H float64) *magnet {
 	// TODO: more initialization methods?
+	// Pad the spins from both ends. Thus the indices are from 1..N
 	if N+2 > spint_bits {
 		return nil
 	}
@@ -37,13 +41,20 @@ func New1DIsing(spins spint, N uint, J, H float64) *magnet {
 	m3 := make([]spint, N)
 	for i := range m1 {
 		shift = spint(i)
-		m1[i] = 1 << shift
+		m1[i] = 2 << shift
 		m2[i] = 3 << shift
 		m3[i] = 7 << shift
 		m |= m1[i]
 	}
+	// -J \sum Si Sj
+	var de_table [8]float64
+	tmp := 4 * J
+	de_table[0] = tmp // 000 => 010: 2 => -2
+	de_table[7] = tmp
+	de_table[2] = -tmp
+	de_table[5] = -tmp
 	mag := &magnet{N: N, J: J, H: H, masks1: m1, masks2: m2, masks3: m3,
-		mask: m}
+		mask: m, dE_table: de_table}
 	mag.Set(spins)
 	return mag
 }
@@ -53,12 +64,19 @@ func (m *magnet) Magnetization() float64 {
 	return float64(2*bits.OnesCount16(m.spins&m.mask) - int(m.N))
 }
 
+// Return the energy difference to flip the i'th spin
+func (m *magnet) DeltaE(i uint) float64 {
+	// idx + 1 % N == i
+	return 0
+}
+
 func (m *magnet) Energy() float64 {
 	// E = -J \sum Si Sj - H \sum Si
 
 	var pair spint
 	var e float64
 	for i := range m.masks1 {
+		// Note here the first pair is spin 1 and spin N
 		pair = (m.spins & m.masks2[i]) >> i
 		if pair == 1 || pair == 2 {
 			e -= 1
@@ -72,10 +90,11 @@ func (m *magnet) Energy() float64 {
 
 // Set spin state
 func (m *magnet) Set(spins spint) {
-	// clear leading digits
+	spins <<= 1
 	spins &= m.mask
-	// set leading 2 digits for PBC
-	spins |= (spins & m.masks2[0]) << m.N
+	// set index 0 and N for PBC
+	spins |= (spins & m.masks1[0]) << m.N
+	spins |= (spins & m.masks1[m.N-1]) >> m.N
 	m.spins = spins
 }
 
